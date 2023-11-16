dbe7386e6444d8dd99a177cdbf2e0a3decfab47c
YuRo <92213643+YuRo711@users.noreply.github.com>
2023-11-16 17:00:14 +0200

    player detection

--- Assets/Scripts/Game/Field/Game Entities/Enemy.cs
+++ Assets/Scripts/Game/Field/Game Entities/Enemy.cs
@@ -34,9 +34,33 @@ public class Enemy : GameEntity
         y = destY;
         transform.SetParent(newCell.transform);
     }
-    
+
     public void CheckForPlayer()
-    {}
+    {
+        var y1 = y;
+        for (var x1 = 0; x1 < LevelGrid.width; x1++)
+        {
+            if (LevelGrid.Cells[x1, y1].GameEntity is PlayerShip)
+            {
+                LookDirection = x1 < x ?
+                    new Direction(-1, 0) :
+                    new Direction(1, 0);
+                return;
+            }
+        }
+
+        var x2 = x;
+        for (var y2 = 0; y2 < LevelGrid.height; y2++)
+        {
+            if (LevelGrid.Cells[x2, y2].GameEntity is PlayerShip)
+            {
+                LookDirection = y2 < y ?
+                    new Direction(0, -1) :
+                    new Direction(0, 1);
+                return;
+            }
+        }
+    }
 
     #endregion
 }
