8175b0e56164446bb6e686675bb1ebeac5a9fd5e
YuRo <92213643+YuRo711@users.noreply.github.com>
2023-12-13 21:02:27 +0200

    asteroid spawn damage

--- Assets/Scripts/Game/Field/Game Entities/Asteroid.cs
+++ Assets/Scripts/Game/Field/Game Entities/Asteroid.cs
@@ -1,4 +1,15 @@
 public class Asteroid : GameEntity
 {
+    #region MonoBehaviour Callbacks
     
+    private void Awake()
+    {
+        health = 1;
+        CollisionInteractions = new()
+        {
+            {typeof(PlayerShip), e => DamageEntity(e, 1, 1)}
+        };
+    }
+
+    #endregion
 }
