54e8a84162c4ad474aa9a2d199dff1020aa21f14
AndreyKuklinov <Andreydolphin@mail.ru>
2023-12-28 03:10:01 +0500

    Remove old details from buttontypes

--- Assets/Scripts/Game/Control Panel/PanelButtonTypes/PanelButtonType.cs
+++ Assets/Scripts/Game/Control Panel/PanelButtonTypes/PanelButtonType.cs
@@ -5,10 +5,6 @@ using UnityEngine.Events;
 public abstract class PanelButtonType : ScriptableObject
 {
     [field: SerializeField] public TurnDirections ActionDirection { get; private set; }
-    [field: SerializeField] public float ChargeMultiplier { get; private set; } = 1f;
-    [field: SerializeField] public bool BreaksAfterTrigger { get; private set; } = true;
-    [field: SerializeField] public bool AutoTriggers { get; private set; } = false;
-    [field: SerializeField] public bool AffectedByMultiplier { get; private set; } = true;
 
     [field: SerializeField] public Sprite BaseSprite { get; private set; }
 
