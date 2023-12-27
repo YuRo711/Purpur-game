6dc1e12a6113471d64769ad557b6742dfb9f42d8
AndreyKuklinov <Andreydolphin@mail.ru>
2023-12-28 03:08:01 +0500

    Implement basic class for button deck

---
+++ Assets/Scripts/Game/Control Panel/ButtonDeck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Control_Panel
+{
+    public class ButtonDeck : MonoBehaviour
+    {
+        [field: SerializeField] public PanelButtonType[] ButtonTypes { get; private set; }
+
+        private Deck<PanelButtonType> buttonDeck;
+
+        public void Start()
+        {
+            buttonDeck = new Deck<PanelButtonType>(ButtonTypes);
+        }
+
+        public PanelButtonType TakeNext()
+        {
+            return buttonDeck.TakeNext();
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Control Panel/Deck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Deck<T>
+{
+    private readonly T[] elements;
+    private Stack<T> currentDeck;
+
+    public Deck(IEnumerable<T> elements)
+    {
+        this.elements = elements.ToArray();
+        ConstructDeck();
+    }
+
+    public T TakeNext()
+    {
+        if (currentDeck.Count == 0)
+            ConstructDeck();
+
+        return currentDeck.Pop();
+    }
+
+    private void ConstructDeck()
+    {
+        var rng = new System.Random();
+        var randomizedList = elements.ToList().OrderBy(x => rng.Next());
+        currentDeck = new Stack<T>(randomizedList);
+    }
+
+    
+
+}
