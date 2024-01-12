2015a4721f7ed915c597571eb3c656df3e2f3958
AndreyKuklinov <Andreydolphin@mail.ru>
2024-01-12 16:30:39 +0500

    Oh my god, I have synced the decks

--- Assets/Scripts/Game/Control Panel/ButtonDeck.cs
+++ Assets/Scripts/Game/Control Panel/ButtonDeck.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Pun;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,12 +11,14 @@ namespace Assets.Scripts.Game.Control_Panel
     public class ButtonDeck : MonoBehaviourPunCallbacks
     {
         [field: SerializeField] public PanelButtonType[] ButtonTypes { get; private set; }
-        [field: SerializeField] public PanelButtonType LastDrawnButton { get; private set; }
+        public Queue<PanelButton> RequestQueue { get; private set; }
 
         private Deck<int> indexDeck;
 
         void Start()
         {
+            RequestQueue = new Queue<PanelButton> ();
+
             if (PhotonNetwork.IsMasterClient)
             {
                 var digits = Enumerable.Range(0, ButtonTypes.Length);
@@ -23,18 +26,19 @@ namespace Assets.Scripts.Game.Control_Panel
             }
         }
 
-        public void RequestNextButton()
+        public void RequestNextButton(PanelButton button)
         {
             if (PhotonNetwork.IsMasterClient)
             {
                 var ind = TakeNextButtonIndex();
                 if (ind == null)
                     return;
-                LastDrawnButton = ButtonTypes[ind.Value];
+                button.ButtonType = ButtonTypes[ind.Value];
             }
             else
             {
-                Debug.Log("[1/3] Calling master for buttons");
+                //Debug.Log("[1/3] Calling master for buttons");
+                RequestQueue.Enqueue(button);
                 photonView.RPC("RequestButtonFromMasterClient", RpcTarget.MasterClient, PhotonNetwork.LocalPlayer.ActorNumber);
             }
         }
@@ -42,7 +46,7 @@ namespace Assets.Scripts.Game.Control_Panel
         [PunRPC]
         public void RequestButtonFromMasterClient(int requestingClientActorNumber)
         {
-            Debug.Log("[2/3] Master responding");
+            //Debug.Log("[2/3] Master responding");
             var ind = TakeNextButtonIndex();
             if (ind == null)
                 return;
@@ -52,8 +56,9 @@ namespace Assets.Scripts.Game.Control_Panel
         [PunRPC]
         public void ReceiveButton(int buttonIndex)
         {
-            Debug.Log("[3/3] Button received");
-            LastDrawnButton = ButtonTypes[buttonIndex];
+            //Debug.Log("[3/3] Button received");
+            var button = RequestQueue.Dequeue();
+            button.ButtonType = ButtonTypes[buttonIndex];
         }
 
         private int? TakeNextButtonIndex()
--- Assets/Scripts/Game/Control Panel/PanelButton.cs
+++ Assets/Scripts/Game/Control Panel/PanelButton.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Game.Control_Panel;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,7 +6,7 @@ using UnityEngine;
 
 public class PanelButton : MonoBehaviour
 {
-    [field: SerializeField] public PanelButtonType ButtonType { get; private set; }
+    [field: SerializeField] public PanelButtonType ButtonType { get; set; }
     [field: SerializeField] public float CurrentCharge { get; private set; }
 
     public ControlPanel ControlPanel { get; private set; }
@@ -47,12 +48,10 @@ public class PanelButton : MonoBehaviour
 
     private void UpdateType()
     {
-        if (ButtonType != null || ControlPanel.PlayerShip == null)
+        if (ButtonType != null || ControlPanel.PlayerShip == null || ControlPanel.PlayerShip.ButtonDeck.RequestQueue.Contains(this))
             return;
 
-        var buttonDeck = ControlPanel.PlayerShip.ButtonDeck;
-        buttonDeck.RequestNextButton();
-        ButtonType = buttonDeck.LastDrawnButton;
+        ControlPanel.PlayerShip.ButtonDeck.RequestNextButton(this);
     }
 
     private void UpdateCharge()
