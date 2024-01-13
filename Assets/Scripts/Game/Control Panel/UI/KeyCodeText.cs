536f35ec686c2fb3b40bd9ceea136cf30046c205
AndreyKuklinov <Andreydolphin@mail.ru>
2024-01-13 08:51:22 +0500

    Add keycode text (needs fixing)

---
+++ Assets/Scripts/Game/Control Panel/UI/KeyCodeText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.Control_Panel.UI
+{
+    public class KeyCodeText : MonoBehaviour
+    {
+        private PanelButton parentButton;
+        private TextMeshProUGUI textMesh;
+
+        void Start()
+        {
+            parentButton = GetComponentInParent<PanelButton>();
+            textMesh = GetComponent<TextMeshProUGUI>();
+        }
+
+        void Update()
+        {
+            textMesh.text = ((char)(parentButton.KeyCode)).ToString().ToUpper();
+        }
+    }
+}
