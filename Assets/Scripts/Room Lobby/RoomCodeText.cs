11a501234ce4f0e8d04a033ea9bda19b57482c1f
AndreyKuklinov <Andreydolphin@mail.ru>
2024-01-15 13:24:25 +0500

    Show room code in text

---
+++ Assets/Scripts/Room Lobby/RoomCodeText.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Room_Lobby
+{
+    public class RoomCodeText : MonoBehaviour
+    {
+        private TextMeshProUGUI text;
+
+        void Start()
+        {
+            text = GetComponent<TextMeshProUGUI>();
+            text.text = PhotonNetwork.CurrentRoom.Name;
+        }
+    }
+}
